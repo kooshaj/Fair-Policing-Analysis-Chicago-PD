6d424cd40bc7d1d2d62cbb66f9d60788eed61756
David Davila <ddavilag@ucsd.edu>
2021-02-17 18:26:12 -0800

    Updated Kaplan Meier Analysis by Gender and Race

--- code/KaplanMeierAnalysis-v01.R
+++ code/KaplanMeierAnalysis-v01.R
@@ -20,7 +20,16 @@ summary(fit_1)
 
 #Plotting the Kaplan Meier Curve by Gender
 library(survminer)
-ggsurvplot(fit_1, data = KaplanMeier, pval = T, xlim = c(0,45), censor.shape = 124)
+ggsurvplot(fit_1, data = KaplanMeier, palette = c("red", "blue"), break.x.by = 5,
+           conf.int = T, pval = T, 
+           surv.median.line = "hv", 
+           title = "", xlab = "Years Served", ylab = "Kaplan-Meier Probability",
+           legend = c(0.8, 0.7), 
+           legend.title = "Groups:",
+           legend.labs = c("Female", "Male"),
+           xlim = c(0,45),
+           pval.method = T,
+           censor = F)
 
 
 
@@ -38,12 +47,45 @@ ggsurvplot(fit_2, data = whiteVsBlack, pval = T, xlim = c(0,45))
 
 
 #Making subsets of data based on race
-whiteVsHispanic = subset(KaplanMeier, KaplanMeier$race == 'WHITE' | KaplanMeier$race == "HISPANIC")
-#Fit the Kaplan Meier curve by White and Hispanic
-surv_object = survival::Surv(time = whiteVsHispanic$years_served, event = whiteVsHispanic$event)
-fit_2 = survminer::surv_fit(surv_object ~ whiteVsHispanic$race, data = whiteVsHispanic)
-summary(fit_2)
+#whiteVsHispanic = subset(KaplanMeier, KaplanMeier$race == 'WHITE' | KaplanMeier$race == "HISPANIC")
+Hispanic = subset(KaplanMeier, KaplanMeier$race == "HISPANIC")
 
-#Plotting the Kaplan Meier Curve by White and Hispanic
+#Fit the Kaplan Meier curve by White and Hispanic
+
+#surv_object = survival::Surv(time = whiteVsHispanic$years_served, event = whiteVsHispanic$event)
+#fit_3 = survminer::surv_fit(surv_object ~ whiteVsHispanic$race, data = whiteVsHispanic)
+#summary(fit_3)
+surv_object = survival::Surv(time = Hispanic$years_served, event = Hispanic$event)
+fit_3 = survminer::surv_fit(surv_object ~ Hispanic$race, data = Hispanic)
+summary(fit_3)
+
+
+
+#Plotting the Kaplan Meier Curve by White Hispanic
 library(survminer)
-ggsurvplot(fit_2, data = whiteVsHispanic, pval = T, xlim = c(0,45))
+ggsurvplot(fit_3, data = Hispanic, pval = T, 
+           combine = T, xlim = c(0,45))
+
+
+
+#Making subsets of data based on race
+whiteVsBlackVsHispanic = subset(KaplanMeier, KaplanMeier$race == 'WHITE' | KaplanMeier$race == "BLACK" | KaplanMeier$race == "HISPANIC")
+#Fit the Kaplan Meier curve by White, Hispanic and Black
+surv_object = survival::Surv(time = whiteVsBlackVsHispanic$years_served, event = whiteVsBlackVsHispanic$event)
+fit_4 = survminer::surv_fit(surv_object ~ whiteVsBlackVsHispanic$race, data = whiteVsBlackVsHispanic)
+summary(fit_4)
+
+#Plotting the Kaplan Meier Curve by White, Hispanic and Black
+library(survminer)
+#ggsurvplot(fit_4, data = whiteVsBlackVsHispanic, pval = T, xlim = c(0,45))
+ggsurvplot(fit_4, data = whiteVsBlackVsHispanic, 
+           palette = c("orange", "green", "blue"), break.x.by = 5,
+           conf.int = T, pval = T, 
+           surv.median.line = "hv", 
+           title = "", xlab = "Years Served", ylab = "Kaplan-Meier Probability",
+           legend = c(0.8, 0.7), 
+           legend.title = "Groups:",
+           legend.labs = c("Black", "Hispanic", "White"),
+           xlim = c(0,45),
+           pval.method = T,
+           censor = F)
